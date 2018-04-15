06563e553f6fe208649b028a3e54b7c1e0be4662
Jessica Robinson <jessica.polansky@gmail.com>
2018-04-15 21:02:42 +0000

    first commit

---
+++ persweb.py
@@ -0,0 +1,36 @@
+import tornado.ioloop
+import tornado.web
+import os
+from jinja2 import \
+Environment, PackageLoader, select_autoescape
+ENV = Environment(
+  loader=PackageLoader('myapp', 'templates'),
+  autoescape=select_autoescape(['html', 'xml'])
+)
+class TemplateHandler(tornado.web.RequestHandler):
+  def render_template (self, tpl, context):
+    template = ENV.get_template(tpl)
+    self.write(template.render(**context))
+class MainHandler(TemplateHandler):
+  def get(self):
+    self.set_header(
+      'Cache-Control',
+      'no-store, no-cache, must-revalidate, max-age=0')
+    self.render_template("home.html", {'name': 'World'})
+def make_app():
+  return tornado.web.Application([
+    (r"/", MainHandler),
+    (
+      r"/static/(.*)", 
+      tornado.web.StaticFileHandler, 
+      {'path': 'static'}
+    ),
+  ], autoreload=True)
+  
+if __name__ == "__main__":
+  tornado.log.enable_pretty_logging
+  
+  app = make_app()
+  PORT = int(os.environ.get('PORT', 8080))
+  app.listen(PORT)
+  tornado.ioloop.IOLoop.current().start()
